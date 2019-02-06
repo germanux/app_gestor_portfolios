7a8040da4bb18d7c84cb9d015c3e098bd629cc82
German Caballero <germanux@gmail.com>
2019-02-06 14:53:57 +0100

    Creando componente de alta.

---
+++ app-cli-ges-port/src/app/form-alta/form-alta.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-form-alta',  // <app-form-alta> Etiqueta </app-form-alta>
+  templateUrl: './form-alta.component.html',
+  styleUrls: ['./form-alta.component.css']
+})
+export class FormAltaComponent implements OnInit {
+  titulo : string;
+  etiqueta_nombre: string;
+  placeholder_nombre: string;
+  nombreIntroducido : string;
+  descripcionIntroducida : string;
+  ficheroIntroducido : string;
+
+  constructor() { }
+// Método, correspondiente al evento "Al inicializar este Componente", es decir, recibe una llamada automática por parte del motor de Angular
+  ngOnInit() {  
+    this.titulo = "Alta de portolios";
+    this.etiqueta_nombre = "Nombre";
+    this.placeholder_nombre = "Introduzca Nombre y apellidos";
+    this.nombreIntroducido = "El Innombrable";
+  }
+  limpiar() {
+    this.nombreIntroducido = "";
+    this.descripcionIntroducida = "";
+    this.ficheroIntroducido = "";
+  }
+  darDeAlta() {
+    // Lo justo para llamar al servicio
+  }
+}
